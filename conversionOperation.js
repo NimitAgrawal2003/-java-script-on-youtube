9fe533f95dacbf6e8ecd9d85b95948700bfc96ab
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-06-26 22:55:05 +0530

    intial commit

---
+++ conversionOperation.js
@@ -0,0 +1,65 @@
+let score = 33
+
+console.log(typeof score);
+let score1 = "33abc"
+console.log(typeof score1);
+
+let valueInNumber = Number (score1)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  // gives output 
+
+// conversion to int(Number)
+// "33abc"  => NaN
+// true => 1; false => 0
+// null => 0
+// true => 1 and false =>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// conversion to boolean
+// 1 => true , 0 =>false
+// " " => false
+// "hitesh" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+//******************operations*********************/
+
+let value = 3 
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // 2 ki power 3
+console.log(2/2);
+console.log(2%3);  // remainder
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let num1,num2,num3
+num1 = num2 = num3 = 4
+
+let game = 100
+game++;
+console.log(game); // prefix and postfix is there
+
+
+
