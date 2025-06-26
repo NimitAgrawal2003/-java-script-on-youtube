f53efcb5c019947f6c3912b7320406757e6e5cf5
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-06-26 21:37:44 +0530

    changes made by adding some more data

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -1,5 +1,24 @@
 "use strict"; // treat all js code as newer version
 
 alert(3+3) // we are using node js not browser
-
 // console.log(3+3) console.log("Hitesh")  gives error
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string single' ' or double" " cought both are valid
+ // boolean => true or false
+ // null => standalone value or empty
+ // undefined =>
+// symbol => unique
+
+
+ // object
+
+console.log(typeof"hitesh");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
