c6ceb8b492e2e66d0ccc7eb35df363d99013889e
NimitAgrawal2003 <agrawalnimit03@gmail.com>
2025-06-26 23:15:25 +0530

    intial commit

---
+++ 04_comparison.js
@@ -0,0 +1,7 @@
+console.log(2>1); // same operations like c++
+
+console.log("2" >1);  // java script will convert it into string into integer
+console.log("02" >1); // prefer to convert it into same data type 
+
+// triple equal check
+console.log("2" === 2); //check that both have same data type or not
