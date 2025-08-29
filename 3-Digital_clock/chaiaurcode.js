5529355c05f263a96491363de0093030fa7e8aa8
Nimit Agrawal <agrawalnimit03@gmail.com>
2025-08-29 20:06:40 +0530

    final commit

--- 3-Digital_clock/chaiaurcode.js
+++ 3-Digital_clock/chaiaurcode.js
@@ -1,4 +1,7 @@
-document.getElementById('clock')
+const clock = document.getElementById("clock");
 
-const date = new Date()
-console.log(date.toLocaleTimeString());
+setInterval(function () {
+  let date = new Date();
+  console.log(date.toLocaleTimeString());
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
